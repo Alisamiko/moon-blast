9f3e3beb88aef7b446cf4b50a850ba6939552a76
Alisamiko <almiko.ivaa@gmail.com>
2026-06-15 18:54:55 +0200

    Add game files

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'moonblast-v1';
+var CACHE_URLS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.map(function(name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(function() {
+        return caches.match('.');
+      });
+    })
+  );
+});
