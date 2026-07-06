34528646ec9402f942c03a2763b08f150ff248ec
Alisamiko <almiko.ivaa@gmail.com>
2026-07-06 20:15:47 +0200

    Добавлен GitHub Actions workflow для деплоя на Pages, обновлена версия кэша

---
+++ sw.js
@@ -0,0 +1,55 @@
+var CACHE_NAME = 'moonblast-v3';
+var CACHE_URLS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.map(function(name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        if (event.request.method === 'GET' && event.request.url.indexOf('http') === 0) {
+          cache.put(event.request, clone);
+        }
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request).then(function(cached) {
+        return cached || caches.match('.');
+      });
+    })
+  );
+});
